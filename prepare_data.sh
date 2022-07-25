18fb74f6a453fdb432cfd6743de30343ab0e3d2c
Kaidong Zhang <34183663+hitachinsk@users.noreply.github.com>
2022-07-25 17:19:28 +0800

    Add prepare data script

---
+++ prepare_data.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+unzip FGT_data.zip
+unzip FGT_weights.zip
+mkdir FGT/checkpoint
+mkdir FGT/flowCheckPoint
+mkdir LAFC/checkpoint
+mv weights/fgt/* FGT/checkpoint
+mv weights/lafc/* LAFC/checkpoint
+mv weights/lafc_single/* FGT/flowCheckPoint
+rm -r weights
